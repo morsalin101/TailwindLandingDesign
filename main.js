4064b8860430b192ac7e31aab7a22c9197599db2
Md.Morsalin <57835317+morsalin101@users.noreply.github.com>
2025-05-05 02:36:28 +0600

    gb

--- main.js
+++ main.js
@@ -12,3 +12,46 @@ closeMenuBtn.addEventListener("click", () => {
     mobileMenu.classList.remove("active");
     document.body.classList.remove("overflow-hidden");
 });
+
+
+
+
+
+    const initialTranslateLTR = -48 * 4;
+    const initialTranslateRTL = 36 * 4;
+
+    function setupIntersectionObserver(element, isLTR, speed) {
+        const intersectionCallback = (entries) => {
+            const isIntersecting = entries[0].isIntersecting;
+            if (isIntersecting) {
+                document.addEventListener('scroll', scrollHandler);
+            } else {
+                document.removeEventListener('scroll', scrollHandler);
+            }
+        };
+        const intersectionObserver = new IntersectionObserver(intersectionCallback);
+
+        intersectionObserver.observe(element);
+
+        function scrollHandler() {
+            const translateY = window.innerHeight - element.getBoundingClientRect().top;
+            const translateX = translateY * speed;
+
+            let totalTranslate = 0;
+            if (isLTR) {
+                totalTranslate = translateX + initialTranslateLTR;
+            } else {
+                totalTranslate = -(translateX + initialTranslateRTL);
+            }
+
+            element.style.transform = `translateX(${totalTranslate}px)`;
+        }
+    }
+
+ 
+    const line4Element = document.getElementById('line4'); 
+
+    if (line4Element) {
+        setupIntersectionObserver(targetLine, true, 0.8); // Apply fast left-to-right functionality
+    }
+
